6e0e062431df0642954a7b639548df29fb50cff3
tylercws <tylercws@outlook.com>
2021-08-03 10:37:37 +0100

    Added basic form

--- app.rb
+++ app.rb
@@ -9,7 +9,19 @@ get '/secret' do
   "You found the secret!"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Misty", "Almond"].sample
+  erb :index
+end
+
+get '/named-cat' do
+  "<form action='/my-handling-form-page' method='post'>
+  <li>
+    <label for='name'>Name:</label>
+    <input type='text' id='name' name='cat_name'>
+  </li>
+  </form>"
+  p params
+  @name = params[:name]
   erb :index
 end
